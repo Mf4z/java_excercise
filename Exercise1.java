49cd8980afd799c9fab4ffc2a7fa3fa17ace006c
Mf4z <emef4z@gmail.com>
2018-01-22 12:41:10 +0100

    Think Java Chapter 1 Exercise

---
+++ Exercise1.java
@@ -0,0 +1,26 @@
+/*
+ * Name : Think Java chapter 1, exercise 1
+ * Written by : Mf4z
+ * Date : 18/1/2018
+ * Program : Hello world and intro
+ * 
+ * */
+
+
+package chapter1;
+
+public class Exercise1 {
+
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World!");
+		
+		System.out.println("How are you?");
+
+		//THis is a line comment
+		
+		
+	}
+
+}
